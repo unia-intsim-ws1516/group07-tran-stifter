f42cb7e3603f67a72f2cc31284a3d58832a6ab83
unknown <stiftech@oclab9.informatik.uni-augsburg.de>
2016-01-11 16:44:54 +0100

    update

---
+++ Unity/mosquito isle/Assets/scripts/QuitApplication.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitApplication : MonoBehaviour {
+
+    // Use this for initialization
+	void Start () {
+	
+	}
+
+    public void ExitApplication()
+    {
+        Application.Quit();        
+    }
+
+
+// Update is called once per frame
+void Update () {
+
+    }
+}
