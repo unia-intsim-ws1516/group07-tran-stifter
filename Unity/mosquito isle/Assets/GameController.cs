99ca9aaf6f224f6a25552dc2655cf0d45659b078
unknown <stiftech@oclab9.informatik.uni-augsburg.de>
2016-01-12 12:31:22 +0100

    gui menu

---
+++ Unity/mosquito isle/Assets/GameController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    private GameObject guiMenu;
+    private Camera camWithoutBreath;
+    public MosquitoMovement mosqMovement;
+
+    private bool menuActive = true;
+    private int currentMask=0;
+
+
+	// Use this for initialization
+	void Awake () {
+        Debug.Log("gc");
+        guiMenu = GameObject.FindWithTag("GUIMenu");
+        camWithoutBreath = Camera.main;
+
+        currentMask = camWithoutBreath.cullingMask;
+        camWithoutBreath.cullingMask = (1 << LayerMask.NameToLayer("Default") | 1 << LayerMask.NameToLayer("TransparentFX") |
+                    1 << LayerMask.NameToLayer("IgnoreRaycast") | 1 << LayerMask.NameToLayer("Water") | 1 << LayerMask.NameToLayer("UI") |
+                    1 << LayerMask.NameToLayer("basic"));
+
+        guiMenu.SetActive(true);
+        mosqMovement.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log("gc_update");
+        if( Input.GetKeyDown(KeyCode.Escape) )
+        {
+            if( menuActive == false )
+            {
+                guiMenu.SetActive(true);
+                mosqMovement.enabled = false;
+                currentMask = camWithoutBreath.cullingMask;
+                camWithoutBreath.cullingMask = (1 << LayerMask.NameToLayer("Default") | 1 << LayerMask.NameToLayer("TransparentFX") | 
+                    1 << LayerMask.NameToLayer("IgnoreRaycast") | 1 << LayerMask.NameToLayer("Water") | 1 << LayerMask.NameToLayer("UI") | 
+                    1 << LayerMask.NameToLayer("basic"));
+            }
+            else
+            {
+                guiMenu.SetActive(false);
+                mosqMovement.enabled = true;
+                camWithoutBreath.cullingMask = currentMask;
+            }
+            menuActive = !menuActive;
+        }
+	}
+}
